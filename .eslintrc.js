4ec5e079ca8f427c5a8474e79288ebff2f44d2b0
abcang <abcang1015@gmail.com>
2024-06-16 17:34:06 +0900

    Remove eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json'
+    project: "./tsconfig.json",
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
     "prettier",
   ],
   plugins: ["@typescript-eslint"],
   rules: {},
-}
+};
